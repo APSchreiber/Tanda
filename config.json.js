4670d2da8994cb23ce1a9dd297dc0fd20c0f1678
Andrew Schreiber <schreiber.aps@gmail.com>
2019-08-03 08:34:22 -0500

    Create config.json.js
    Added config

---
+++ config.json.js
@@ -0,0 +1,24 @@
+var musicSiteConfig = {
+	
+	artist: {
+		name: "Catching the Westbound",
+		description: "St. Louis based old time folk, blues, and mountain gospel",
+		email: "music@catchingthewestbound.com",
+		soundcloudId: "catchthewest",
+		links: {
+			twitter: "https://twitter.com/catchthewest",
+			soundcloud: "https://soundcloud.com/catchthewest",
+			youtube: "https://www.youtube.com/channel/UCeeRxS5wGE2UutrNrBv5GsA",
+			facebook: "",
+			instagram: ""
+		},
+		bio: "Transplants to St. Louis from Arizona and Ohio, Emily Stuart and Andy Schreiber blend together elements of rural American music. Drawing from a catalog of original songs and 19th and 20th century folk standards, the duet weaves together syncopated string rhythms of country-blues guitar with vocal melodies that evoke the hardships and hopefulness of Appalachian Mountain gospel."
+	},
+	
+	geo: {
+		states: ["IL", "MO"],
+		defaultState: "MO",
+		defaultCountry: "USA"
+	}
+	
+}
