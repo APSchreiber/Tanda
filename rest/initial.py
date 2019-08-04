69d340a29d87c3b4806ca2bc2e0d8c50579ad174
Andrew Schreiber <schreiber.aps@gmail.com>
2019-08-03 19:28:42 -0500

    Added additional tables to initial db

--- rest/initial.py
+++ rest/initial.py
@@ -3,9 +3,23 @@ import sqlite3
 con = sqlite3.connect('tanda.db')
 
 # People
-con.execute("CREATE TABLE people (id INTEGER PRIMARY KEY AUTOINCREMENT, last TEXT NOT NULL, first TEXT, middle Text, suffix TEXT, email TEXT, phone TEXT, description TEXT, dob TEXT)")
+con.execute("CREATE TABLE people (id INTEGER PRIMARY KEY AUTOINCREMENT, eto INTEGER, first TEXT, last TEXT NOT NULL, middle Text, suffix TEXT, email TEXT, phone TEXT, description TEXT, comments TEXT, dob TEXT, address INTEGER, FOREIGN KEY(address) REFERENCES places(id))")
 con.execute("INSERT INTO people (last, first) VALUES ('Stuart', 'Emily')")
 
+con.execute("CREATE VIEW people_vw AS SELECT people.id, people.first, people.last, people.middle, people.suffix, people.email, people.phone, people.description, people.dob, strftime('%m/%d/%Y', people.dob) as dob_format places.address1, places.address2, places.city, places.state, places.zip, places.country, places.description as placedescription FROM people INNER JOIN places on people.address = places.id)
 
+# Places
+con.execute("CREATE TABLE places (id INTEGER PRIMARY KEY AUTOINCREMENT, address1 TEXT, address2 TEXT, city TEXT, state TEXT, zip TEXT, country TEXT, description TEXT, comments TEXT)")
+con.execute("INSERT INTO places (address1, city, state, zip, country) VALUES ('12 Ola Ave.', 'St. Louis', 'MO', '63119', 'USA')")
+
+# Circles
+con.execute("CREATE TABLE circles (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, start TEXT, months INTEGER, due INTEGER, loan INTEGER, capacity INTEGER, description TEXT, comments TEXT)")
+con.execute("CREATE TABLE circles_people (circleid INTEGER NOT NULL, peopleid INTEGER NOT NULL, payout_order INTEGER, distribution TEXT, PRIMARY KEY(circleid, peopleid))")
+
+# Accounts
+con.execute("CREATE TABLE accounts (id INTEGER PRIMARY KEY AUTOINCREMENT, accountno TEXT, comments TEXT, person INTEGER, FOREIGN KEY(person) REFERENCES people.id)")
+
+# Payments
+con.execute("CREATE TABLE payments (id INTEGER PRIMARY KEY AUTOINCREMENT, amount INTEGER NOT NULL, date TEXT, person INTEGER, account INTEGER, circle INTEGER, FOREIGN KEY(account) REFERENCES accounts.id)")
 
 con.commit()
